27a2a36e3e90497931b08277e5d966409a8a9bdb
Edvin Aginger <edvinaginger@hotmail.com>
2019-10-16 09:13:01 +0200

    photopage v.2

---
+++ script.js
@@ -0,0 +1,19 @@
+
+/*   var rellax = new Rellax('.rellax'); */
+/* 
+var edvin = {
+    name: "Edvin",
+    age: 21,
+    other: "other"
+};
+
+if (edvin.age >= 18) {
+    console.log(" " + edvin.name + " får kolla porr");
+}
+else if (edvin.age <= 18) {
+    console.log(edvin.name + " får INTE kolla porr");
+}
+
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+} */
