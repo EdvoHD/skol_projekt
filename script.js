3791bea0035a8f262348cd407d7100ecb3d286cf
Edvin Aginger <edvinaginger@hotmail.com>
2019-10-18 20:14:47 +0200

    Only Index is relevant page, rest is for testing

--- script.js
+++ script.js
@@ -16,4 +16,55 @@ else if (edvin.age <= 18) {
 
 for (let i = 10; i > 0; i--) {
     console.log(i);
-} */
+    */
+
+
+
+
+
+
+// Switch content 
+
+    // Knappar 
+
+
+
+function contentSwitch1() {
+    // Image switch
+    document.getElementById("extras-image").src = "images/compressed/burger_image.jpg";
+    document.getElementById("extras-h2").innerHTML = "Dynamic content";
+    document.getElementById("extras-p").innerHTML = "Click the menu buttons to the see this content switch around. Super fancy Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, excepturi!";
+}
+
+function contentSwitch2() {
+    // Image switch
+    document.getElementById("extras-image").src = "images/compressed/car_image.jpg"
+    document.getElementById("extras-h2").innerHTML = "Titel 2";
+    document.getElementById("extras-p").innerHTML = "utbytt text för p version 2 PRETTY COOL HUH?";
+}
+
+function contentSwitch3() {
+    // Image switch
+    document.getElementById("extras-image").src = "images/compressed/sunset_image.jpg";
+    document.getElementById("extras-h2").innerHTML = "Titel3";
+    document.getElementById("extras-p").innerHTML = "utbytt text för p version 2 PRETTY COOL HUH? Ännu mera text, det är rätt så schysst";
+}
+
+
+
+
+
+/* function calcmoms(prodPrice) {
+
+    let total = prodPrice + (prodPrice*.25)
+    console.log(prodPrice*.25);
+    return total;
+}
+
+let firstProduct = calcmoms(2000);
+let secondProduct = calcmoms(550);
+
+const calc = function () {
+    console.log("räknar något")
+}
+ */
