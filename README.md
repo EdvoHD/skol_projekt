2ff9c8310f9dc1cd24804db723ce5024bc48658a
Edvin Aginger <edvinaginger@hotmail.com>
2019-10-19 11:33:08 +0200

    Final push, all files sorted, cleaned up code, made comments, added instructions for README. Everything done for now

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# skol_prjekt
+# skol_projekt
+
+Open "index.html" for viewing and reviewing my recent project.
+The stylesheet for it is "main.css".
+"script.js" is necessary for a contentswitch function on the page.
+
+The page is built to last and is modular in a few ways. You can add and remove content and sections without affecting the entire page (unless you remove the body ;) ).
+
+For example: The ".tl-element" is a element within my ".timeline" - section that switches sides of the timeline for each new item. You can copy+paste as many of ".tl-element" as you want below eachother and it will still work, therefore = modular and scaleable.
+
+
+
--- script.js
+++ script.js
@@ -1,37 +1,8 @@
-
-/*   var rellax = new Rellax('.rellax'); */
-/* 
-var edvin = {
-    name: "Edvin",
-    age: 21,
-    other: "other"
-};
-
-if (edvin.age >= 18) {
-    console.log(" " + edvin.name + " får kolla porr");
-}
-else if (edvin.age <= 18) {
-    console.log(edvin.name + " får INTE kolla porr");
-}
-
-for (let i = 10; i > 0; i--) {
-    console.log(i);
-    */
-
-
-
-
-
-
-// Switch content 
-
-    // Knappar 
-
-
-
+// Switch content for the #extras - section
 function contentSwitch1() {
     // Image switch
-    document.getElementById("extras-image").src = "images/compressed/burger_image.jpg";
+    document.getElementById("extras-image").src = "images/compressed/sunset_image.jpg";
+    // Text switch
     document.getElementById("extras-h2").innerHTML = "Dynamic content";
     document.getElementById("extras-p").innerHTML = "Click the menu buttons to the see this content switch around. Super fancy Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, excepturi!";
 }
@@ -39,32 +10,15 @@ function contentSwitch1() {
 function contentSwitch2() {
     // Image switch
     document.getElementById("extras-image").src = "images/compressed/car_image.jpg"
-    document.getElementById("extras-h2").innerHTML = "Titel 2";
-    document.getElementById("extras-p").innerHTML = "utbytt text för p version 2 PRETTY COOL HUH?";
+    // Text switch
+    document.getElementById("extras-h2").innerHTML = "Switched image";
+    document.getElementById("extras-p").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, excepturi!";
 }
 
 function contentSwitch3() {
     // Image switch
-    document.getElementById("extras-image").src = "images/compressed/sunset_image.jpg";
-    document.getElementById("extras-h2").innerHTML = "Titel3";
+    document.getElementById("extras-image").src = "images/compressed/burger_image.jpg";
+    // Text switch
+    document.getElementById("extras-h2").innerHTML = "A new title again";
     document.getElementById("extras-p").innerHTML = "utbytt text för p version 2 PRETTY COOL HUH? Ännu mera text, det är rätt så schysst";
 }
-
-
-
-
-
-/* function calcmoms(prodPrice) {
-
-    let total = prodPrice + (prodPrice*.25)
-    console.log(prodPrice*.25);
-    return total;
-}
-
-let firstProduct = calcmoms(2000);
-let secondProduct = calcmoms(550);
-
-const calc = function () {
-    console.log("räknar något")
-}
- */
